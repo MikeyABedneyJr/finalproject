0912279a3a3a377f43454b755d8c05fd3a2b7613
Mike (Mikey) Bedney <mike.bedney@gmail.com>
2014-11-30 16:24:41 -0800

    Introducing shuffle button
    There's too much more to do and I need to get this thing working---so I
    took a short cut of introducing a shuffle button (thankfully works).
    Now I can focus on slowing the sort down and hopefully animating them as
    they switch positions.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,37 @@
 $(document).ready(function() {
 
-    var kidpositions = ['brad.jpg', 'jill.jpg', 'kat.jpg', 'mal.jpg', 'sam.jpg', 'zach.jpg'];
-    
-    //randomize kid location upon pageload
-    var i = 1;
-    while (i <= kidpositions.length) {
-        $('<img src="images/' + kidpositions[Math.floor(Math.random() * kidpositions.length)] + '">').appendTo('#studentorder');
-        i++;
+    var numbers = [0,1,2,3,4,5, 6];
+
+
+//Shuffle function after clicking button:  http://bost.ocks.org/mike/shuffle/
+    $("#button1").click(function() {
+    var m = numbers.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+        // And swap it with the current element.
+        t = numbers[m];
+        numbers[m] = numbers[i];
+        $('#' + i).appendTo('#studentorder');
+        numbers[i] = t;
+        }
+    });
+
+//Sorting button/function
+    $("#button2").click(function() {
+        var tmp;
+        for (var i = 0; i < numbers.length; i += 1) {
+            for (var j = i; j > 0; j -= 1) {
+                if (numbers[j] < numbers[j - 1]) {
+                tmp = numbers[j];
+                numbers[j] = numbers[j - 1];
+                numbers[j - 1] = tmp;
+                };
+            };
+        $('#' + i).appendTo('#studentorder');
     }
+    });
 
 });
